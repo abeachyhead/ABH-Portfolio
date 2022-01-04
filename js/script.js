bf8f7a0b5d0df5a975e841aa46f40128a19f060c
abeachyhead <amybeachyhead@gmail.com>
2022-01-04 14:31:54 +0200

    sticky nav

--- js/script.js
+++ js/script.js
@@ -33,3 +33,22 @@ allLinks.forEach(function (link) {
       headerEl.classList.toggle("nav-open");
   });
 });
+
+// sticky navigation
+const sectioneHeroEl = document.querySelector(".section-hero");
+
+const obs = new IntersectionObserver(
+  function (entries) {
+    const ent = entries[0];
+
+    if (ent.isIntersecting === false) document.body.classList.add("sticky");
+
+    if (ent.isIntersecting === true) document.body.classList.remove("sticky");
+  },
+  {
+    root: null,
+    threshold: 0,
+    rootMargin: "-70px",
+  }
+);
+obs.observe(sectioneHeroEl);
