8810356eef071bc02ddf3c22eba4a6f4654f107f
abeachyhead <amybeachyhead@gmail.com>
2022-01-04 09:19:13 +0200

    Initialization

---
+++ js/script.js
@@ -0,0 +1,7 @@
+// using hamburger menu on mobile
+const btnNavEl = document.querySelector(".btn-mobile-nav");
+const headerEl = document.querySelector(".header");
+
+btnNavEl.addEventListener("click", function () {
+  headerEl.classList.toggle("nav-open");
+});
