883a3b44010564c40df247c04b378b9f59154cf1
abeachyhead <amybeachyhead@gmail.com>
2022-01-06 10:09:42 +0200

    added contact me section

--- index-js/script.js
+++ index-js/script.js
@@ -7,32 +7,32 @@ btnNavEl.addEventListener("click", function () {
 });
 
 // smooth scrolling animation
-const allLinks = document.querySelectorAll("a:link");
+// const allLinks = document.querySelectorAll("a:link");
 
-allLinks.forEach(function (link) {
-  link.addEventListener("click", function (e) {
-    e.preventDefault();
-    const href = link.getAttribute("href");
+// allLinks.forEach(function (link) {
+//   link.addEventListener("click", function (e) {
+//     e.preventDefault();
+//     const href = link.getAttribute("href");
 
-    // scroll to top
-    if (href === "#")
-      window.scrollTo({
-        top: 0,
-        behavior: "smooth",
-      });
+//     // scroll to top
+//     if (href === "#")
+//       window.scrollTo({
+//         top: 0,
+//         behavior: "smooth",
+//       });
 
-    // scroll to other links
-    if (href !== "#" && href.startsWith("#")) {
-      const sectionEl = document.querySelector(href);
-      sectionEl.scrollIntoView({
-        behavior: "smooth",
-      });
-    }
-    // close mobile navigation
-    if (link.classList.contains("main-nav-link"))
-      headerEl.classList.toggle("nav-open");
-  });
-});
+//     // scroll to other links
+//     if (href !== "#" && href.startsWith("#")) {
+//       const sectionEl = document.querySelector(href);
+//       sectionEl.scrollIntoView({
+//         behavior: "smooth",
+//       });
+//     }
+//     // close mobile navigation
+//     if (link.classList.contains("main-nav-link"))
+//       headerEl.classList.toggle("nav-open");
+//   });
+// });
 
 // sticky navigation
 const sectioneHeroEl = document.querySelector(".section-hero");
